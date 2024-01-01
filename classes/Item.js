bb9ec742d4dc4a7b86aaf5bc1adafcc426c1b1ba
Luiz_E <luizedumartins21@gmail.com>
2024-01-01 01:24:09 -0300

    starting inventory

---
+++ classes/Item.js
@@ -0,0 +1,10 @@
+class Item {
+    name;
+    type;
+
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+}
+
--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-import Location from "./classes/Location.js";
-import Weapon from "./classes/Weapon.js";
 import Town from "./classes/Town.js";
 import Player from "./classes/Player.js";
-import { actions } from "./ui/uiData.js";
+const inventory = document.querySelector("#inventory")
 
 const player = new Player();
 
 function main() {
     Town.changeScenery("town")
+    for (let i = 0; i < 15; i++) {
+        const div = document.createElement("div");
+        div.id = "inventory-space";
+        inventory.appendChild(div);
+    }
 }
 
 export default player;
